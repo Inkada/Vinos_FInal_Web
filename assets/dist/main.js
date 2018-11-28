22a98f12eaa6d6f061dc25141b19350a54605491
DESKTOP-80DLUBQ\DANIEL <danielstevenrojas@hotmail.com>
2018-11-28 12:15:32 -0500

    cambiando los hbs

---
+++ assets/dist/main.js
@@ -0,0 +1,45 @@
+
+document.querySelectorAll('.wineContainer').forEach((e) => {
+
+    e.addEventListener('click', (e) => {
+        document.querySelector('.forPopUp').innerHTML = `
+        <div class="popup">
+        <div class="productContainer">
+
+        <div class="imageContainer">
+            <div class="wineImage" style="background-image:url('./assets/imgs/wines/wine.png')">
+
+            </div>
+        </div>
+        <div class="detailsContainer">
+            <div class="title">
+                <h3>Vino Rojo (800ml)</h3>
+                <h6>A00058176</h6>
+            </div>
+
+            <div class="price">
+                <h6>Precio</h3>
+                    <h3>$25 000</h3>
+            </div>
+
+            <div class="quantity">
+                <h6>Cantidad</h6>
+                <input type="number" name="" id="" value="1">
+            </div>
+
+            <div class="buttonAddBag">
+                <div class="icon">
+                    <img src="./assets/imgs/icos/bag.png" alt="">
+                </div>
+                <p>Agregar a la bolsa</p>
+            </div>
+        </div>
+    </div>
+    </div>
+        `;
+
+        document.querySelector('.buttonAddBag').addEventListener('click', (e) => {
+            document.querySelector('.forPopUp').innerHTML = '';
+        })
+    })
+})
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
-    ObjectID = require('mongodb').ObjectID,
+ObjectID = require('mongodb').ObjectID,
     express = require('express'),
     engines = require('consolidate');
 
@@ -7,74 +7,55 @@ var app = express(),
     db;
 
 app.engine('hbs', engines.handlebars);
-
 app.set('views', './views');
 app.set('view engine', 'hbs');
-
-app.use(express.static('public'));
+app.use(express.static('views'));
 
 var PORT = process.env.PORT || 5000;
 
-
 // Conectarse a Base de Datos
-MongoClient.connect("mongodb+srv://miprimercluster-zfvgq.mongodb.net/productos",
-{
-    auth: { user: "DanielRojas",
+MongoClient.connect("mongodb+srv://miprimercluster-zfvgq.mongodb.net/productos", {
+        auth: {
+            user: "DanielRojas",
             password: "contraseñabasica"
 
-}},
-function(err,client){
-if(err)throw err;
+        }
+    },
+    function (err, client) {
+        if (err) throw err;
 
-db = client.db("productos");
-  
-//Iniciar el servidor
-app.listen(PORT);
+        db = client.db("productos");
 
-}
+        //Iniciar el servidor
+        app.listen(PORT);
+
+    }
 );
 
-
 app.get('/', (req, res) => {
+    const collection = db.collection('productos').find({});
 
-    var prod = db.collection('vinos')
-        .find();
-    
-    if(req.query.marca)
-        prod.filter({ marca: req.query.marca });
+    if (req.query.tipo) {
+        collection.filter({
+            //tipo: "Blanco"
+            tipo: req.query.tipo
+        })
+    }
 
-    if(req.query.modelo)
-        prod.filter({ modelo: req.query.modelo });
+    collection.toArray(function (err, documentos) {
 
-    prod.toArray((err, result) => {
-            console.log('hola servidor')
-            res.render('index', {
-                vinos: result
-            });
-        });
-});
-
-app.get('/checkout', (req, res) => {
-    res.render('checkout');
-});
+        if (err) {
+            console.error(err);
+            res.send(err);
+            return;
+        }
 
 
-app.get('/producto/:id', (req, res) => {
-    db.collection('vinos').find({ modelo: req.params.id }).toArray((err, result) => res.send(result))
-});
+        var contexto = {
+            productos: documentos,
 
+        };
+        res.render("Homepage", contexto);
 
-app.get('/vinosPorIds', (req, res) => {
-    console.log(req.query.ids);
-    var arreglo = req.query.ids.split(',');
-    arreglo = arreglo.map(function(id) {
-        return new ObjectID(id);
     });
-    var prod = db.collection('vinos')
-        .find({ _id: { $in: arreglo } })
-        .toArray((err, result) => {
-            res.send(result);
-        });
-});
-
-//
+})
